232e8ab5a7d1d2c9d32ce8ca09a1920b4188bf56
Kevin Arne <kevin.arne@gmail.com>
2020-04-24 23:31:03 -0400

    Added some more pseudo code and added loops to the keyboard function.

--- src/macroKeyMain/macroKeyMain.ino
+++ src/macroKeyMain/macroKeyMain.ino
@@ -8,5 +8,19 @@ void loop() {
   //if rotary encoder debouncing done
     //Check for rotary encoder input
 
-  //Check for millis wrap around
+  //Check for millis wrap around (nope, use this: https://arduino.stackexchange.com/questions/12587/how-can-i-handle-the-millis-rollover)
+}
+
+void keyboardScan(){
+  //Iterate through rows
+  for (int r = 0; r < 4; r++){
+    //Set row to high
+    for (int c = 0; c < 3; c++){ //Heh c++
+      //Read each column
+      //Update matrix of pressed buttons
+      //If already pressed: don't update last pressed
+      //Else: update last pressed value for that spot. last pressed = millis()
+    }
+    //Set row to low
+  }
 }

719b0dc4d3c1ed8f7bd7aa5a1ec5f06a63e9969f
Kevin Arne <kevin.arne@gmail.com>
2020-04-25 23:16:40 -0400

    Addedmore structure to the keyboard scanning, including some digital reads. Added a toBeSent matrix that helps with communication between the keyboardScanning function and main loop. To do: setup the software serial, add the keyboard sending logic (and storage), and add rotary encoder reading.

--- src/macroKeyMain/macroKeyMain.ino
+++ src/macroKeyMain/macroKeyMain.ino
@@ -1,27 +1,70 @@
-#define ENPIN A1
-#define ROTBUT A2
-#define LCDSER 10
-#define EN_CLK 2
-#define EN_DAT A3
+#define ENA_PIN A1 //Enable pin that disables keyboard output
+#define LCD_SER 10 //Pin for the software serial communication to the LCD panel
+
+#define EN_BUT A2  //Button on rotary encoder
+#define EN_CLK 2 //Clock on rotary encoder
+#define EN_DAT A3 //Data on rotary encoder
+
+#define KEY_DELAY 30 //In millis
+#define EN_DELAY 30 //In millis
+#define SWITCH_DELAY 30 //In millis
+
+//Need variables to store relevant states for rotary encoder
+
+unsigned long lastSwitch = 0;
+unsigned long lastRotation = 0;
 
 
+//rows and cols are the pins associated with the rows and columns of switches
 int rows[] = {3,7,8,9};
-int cols[] = {4,5.6};
+int cols[] = {4,5,6};
+
+//Keeps track of whether a button has been pressed
+boolean pressed[4][3] = {{false,false,false},
+                         {false,false,false},
+                         {false,false,false},
+                         {false,false,false}};
+
+//Keeps track of when a button was last pressed for debouncing
+unsigned long lastPressed[4][3] = {{0,0,0},
+                                   {0,0,0},
+                                   {0,0,0},
+                                   {0,0,0}};
+
+//Keeps track of buttons to be sent
+boolean toBeSent[4][3] = {{false,false,false},
+                         {false,false,false},
+                         {false,false,false},
+                         {false,false,false}};
 
 void setup() {
   //Setup rows as outputs
+  for (int i = 0; i < 4; i++){
+    pinMode(rows[i], OUTPUT);  
+  }
   //Setup cols as inputs
-  //Setup ENPIN as input
-  //Setup ROTBUT as input
-  //Setup ENCLK and ENDAT as inputs
+  for (int i = 0; i < 3; i ++){
+    pinMode(cols[i], OUTPUT);  
+  }
+  //Setup ENA_PIN as input
+  pinMode(ENA_PIN, INPUT);
+  
+  //Setup EN_BUT as input
+  pinMode(EN_BUT, INPUT);
+  //Setup EN_CLK and EN_DAT as inputs
+  pinMode(EN_CLK, INPUT);
+  pinMode(EN_DAT, INPUT);
+
   
   //Start software serial with LCDSER
   //Attach interrupt to ENCLK
 }
 
 void loop() {
-  //if keyboard debouncing done
-    //Check for keyboard input
+  keyboardScan();
+  //Send buttons to be sent
+  //Set buttons to be sent to false
+  
   //if rotary encoder debouncing done
     //Check for rotary encoder input
 
@@ -32,12 +75,26 @@ void keyboardScan(){
   //Iterate through rows
   for (int r = 0; r < 4; r++){
     //Set row to high
+    digitalWrite(rows[r], HIGH);
     for (int c = 0; c < 3; c++){ //Heh c++
       //Read each column
+      int value = digitalRead(cols[c]);
       //Update matrix of pressed buttons
-      //If already pressed: don't update last pressed
+      if (value){
+        //Check the previous value
+        //If already pressed: don't update last pressed
+        //else, do update last pressed 
+          //Update to be sent
+      }else{
+        //Check the previous value
+        //If already pressed
+          //Check time elapsed
+          //Set to false if time elapsed is long enough
+      }
+            
       //Else: update last pressed value for that spot. last pressed = millis()
     }
     //Set row to low
+    digitalWrite(rows[r], LOW);
   }
 }
